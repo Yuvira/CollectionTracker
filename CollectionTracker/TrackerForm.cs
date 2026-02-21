2e929990cd7a883f505c5ec9cdbb7110ed3f7771
Yuvira <retrokitty10@gmail.com>
2026-02-21 04:00:34 -0500

    Move TrackerPage class to its own script

--- CollectionTracker/TrackerForm.cs
+++ CollectionTracker/TrackerForm.cs
@@ -3,37 +3,6 @@ using System.Windows.Forms;
 
 namespace CollectionTracker {
 
-	//Page class
-	public abstract class TrackerPage {
-
-		//Properties
-		protected TrackerForm parent;
-		protected TrackerPanel panel;
-
-		//Constructor
-		public TrackerPage(TrackerForm form) {
-
-			//Generate panel and initialize
-			parent = form;
-			panel = Utils.GenerateTrackerPanel(new Rectangle(Point.Empty, form.Size));
-			panel.Dock = DockStyle.Fill;
-			Init();
-
-			//Replace active panel
-			parent.Page?.RemovePanel();
-			parent.Controls.Add(panel);
-			parent.SetPage(this);
-
-		}
-
-		//Initialize
-		public abstract void Init();
-
-		//Remove panel from form controls
-		public void RemovePanel() => parent.Controls.Remove(panel);
-
-	}
-
 	//Form class
 	public partial class TrackerForm : Form {
 
