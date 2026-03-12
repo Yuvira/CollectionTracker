6f5f08b3084530b175d372050d1c716943cf09a8
Yuvira <retrokitty10@gmail.com>
2026-03-12 04:04:54 -0400

    Prioritize color before identity when coloring MTG panels

--- CollectionTracker/Detailpage.cs
+++ CollectionTracker/Detailpage.cs
@@ -475,8 +475,8 @@ namespace CollectionTracker {
 		//Set panel color
 		private void SetPanelColorsMTG(List<TrackerPanel> panels) {
 			string color = "";
-			if (!printing.Card.TryGetFirstField("identity", out color) || string.IsNullOrWhiteSpace(color))
-				printing.Card.TryGetFirstField("color", out color);
+			if (!printing.Card.TryGetFirstField("color", out color) || string.IsNullOrWhiteSpace(color))
+				printing.Card.TryGetFirstField("identity", out color);
 			color = color.ToLower();
 			foreach (TrackerPanel panel in panels) {
 				if (color.Length == 0)
